456f187c07f138052a92efb670867baecdd2dae9
Ismail-anis-cherrak <ia.cherrak@esi-sba.dz>
2025-11-03 18:06:48 +0100

    Prepare for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Add valid Next.js config options here if needed
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/blog-website',
+  assetPrefix: '/blog-website/',
+
 };
 
 export default nextConfig;
