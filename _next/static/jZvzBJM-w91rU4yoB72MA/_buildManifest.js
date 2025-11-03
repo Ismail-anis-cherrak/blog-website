56a4d063f650cdfb169e961ac8046d1a3ba4ac3b
Ismail-anis-cherrak <ia.cherrak@esi-sba.dz>
2025-11-03 18:40:36 +0100

    Updates

--- _next/static/jZvzBJM-w91rU4yoB72MA/_buildManifest.js
+++ _next/static/jZvzBJM-w91rU4yoB72MA/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/blog-website//_next/:path+",
-        "destination": "/blog-website/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
